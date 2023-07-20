174b57477fbe54cc232272d4ac52cdde0a8a2036
tavaresflavia <altavares.flavia@gmail.com>
2023-07-19 20:55:57 -0400

    sprint2 feedback fixed

--- scripts/build-shows-pages.js
+++ scripts/build-shows-pages.js
@@ -1,114 +1,113 @@
 //Global variables
 
-const SHOWS_ENDPOINT = "https://project-1-api.herokuapp.com/showdates?api_key="
-const API_KEY = "e0eea5f0-0f8c-4b54-9fc4-ff50843766d4"
-let cardsList = document.querySelector('.shows-section__cards');
-
+const SHOWS_ENDPOINT = 'https://project-1-api.herokuapp.com/showdates?api_key=';
+const API_KEY = 'e0eea5f0-0f8c-4b54-9fc4-ff50843766d4';
+const tableEl = document.querySelector('.shows__table');
 
 axios
-    .get(SHOWS_ENDPOINT + API_KEY)
-    .then ((response) => {
-        console.log(response);
-        displayShows (response.data);
-
-    })
-    .catch(() => {
+  .get(SHOWS_ENDPOINT + API_KEY)
+  .then((response) => {
+    displayShows(response.data);
+  })
+  .catch(() => {
     failMsg = document.createElement('p');
-    failMsg.innerText = "We're sorry, but there was an issue loading the data for this section.Please refresh the page to try again.";
-    failMsg.classList.add('shows-section__error-msg');
-    cardsList.appendChild(failMsg);
-}
-    )
+    failMsg.innerText = "We're sorry, but there was an issue loading the data for this section. Please refresh the page to try again.";
+    tableEl.appendChild(failMsg);
+  });
 
 //FUNCTIONS
 
-// Display elements  takes and array of objects, call creatCards and eventlistiner to select 
-function displayShows (array){
+// Display elements  takes and array of objects, call creatCards and eventlistiner to select
+function displayShows(array) {
+  let cardsList = createHeaders();
+  for (let i = 0; i < array.length; i++) {
+    let cardEl = createCards(array[i], cardsList);
 
-for (let i = 0; i < array.length; i++ ){
-
-let cardEl = createCards(array[i]);
-
-
-cardEl.addEventListener('click', (event) => {
-    let selShow = document.querySelector('.card--selected')
-    if (selShow === null){
+    cardEl.addEventListener('click', (event) => {
+      let selShow = document.querySelector('.card--selected');
+      if (selShow === null) {
         cardEl.classList.add('card--selected');
-    }else{
+      } else {
         selShow.classList.remove('card--selected');
 
-        if (cardEl !== selShow){
-            cardEl.classList.add('card--selected');
+        if (cardEl !== selShow) {
+          cardEl.classList.add('card--selected');
         }
+      }
+    });
+  }
+}
 
+function createHeaders() {
+  const headerEl = document.createElement('article');
+  headerEl.classList.add('shows__header');
+  tableEl.appendChild(headerEl);
 
-    }
-    
-    
-})
+  const headDateEl = document.createElement('p');
+  headDateEl.innerText = 'DATE';
+  headDateEl.classList.add('shows__header-label', 'labels');
+  headerEl.appendChild(headDateEl);
 
-}}
+  const headVenueEl = document.createElement('p');
+  headVenueEl.innerText = 'VENUE';
+  headVenueEl.classList.add('shows__header-label', 'labels');
+  headerEl.appendChild(headVenueEl);
 
+  const headLocEl = document.createElement('p');
+  headLocEl.innerText = 'LOCATION';
+  headLocEl.classList.add('shows__header-label', 'labels');
+  headerEl.appendChild(headLocEl);
 
-function createCards(show){
-    //<article class='card'>
-    const cardEl = document.createElement('article');
-    cardEl.classList.add('card');
-    cardsList.appendChild(cardEl);
-    
-    //<p class='card__label labels card__label--top'>DATE</p>
-    const dateLblEl = document.createElement('p');
-    dateLblEl.classList.add('card__label', 'labels');
-    dateLblEl.innerText = 'DATE';
-    cardEl.appendChild(dateLblEl);
-    
-    // <p class='card__value'>Mon Sept 06 2021</p>
-    const dateValEl = document.createElement('p');
-    dateValEl.classList.add('card__value', 'card__value--bold');
-    dateValEl.innerText = new Date(show.date).toDateString();
-    cardEl.appendChild(dateValEl);
-    
-    //<p class='card__label labels card__label--top'>VENUE</p>
-    const venueLblEl = document.createElement('p');
-    venueLblEl.classList.add('card__label', 'labels');
-    venueLblEl.innerText = 'VENUE';
-    cardEl.appendChild(venueLblEl);
-    
-    
-    //<p class='card__value'>Ronald Lane</p>
-    const venueValEl = document.createElement('p');
-    venueValEl.classList.add('card__value');
-    venueValEl.innerText = show.place;
-    cardEl.appendChild(venueValEl);
-    
-    
-    //<p class='card__label labels '>LOCATION</p>
-    const locationLblEl = document.createElement('p');
-    locationLblEl.classList.add('card__label', 'labels');
-    locationLblEl.innerText = 'LOCATION';
-    cardEl.appendChild(locationLblEl);
-    
-    //<p class='card__value'>San Francisco, CA</p>
-    const locationValEl = document.createElement('p');
-    locationValEl.classList.add('card__value');
-    locationValEl.innerText = show.location;
-    cardEl.appendChild(locationValEl);
-    
-    //<a href="" class="card__button button">BUY TICKETS</a>
-    const buttonEl = document.createElement('a');
-    buttonEl.classList.add('card__button', 'button');
-    buttonEl.setAttribute('href','#')
-    buttonEl.innerText = 'BUY TICKETS';
-    cardEl.appendChild(buttonEl);
+  const cardsList = document.createElement('div');
+  cardsList.classList.add('shows__cards');
+  tableEl.appendChild(cardsList);
 
-    return cardEl;
-    
-    }
+  return cardsList;
+}
+
+function createCards(show, cardsList) {
+
+  const cardEl = document.createElement('article');
+  cardEl.classList.add('card');
+  cardsList.appendChild(cardEl);
+
+  const dateLblEl = document.createElement('p');
+  dateLblEl.classList.add('card__label', 'labels');
+  dateLblEl.innerText = 'DATE';
+  cardEl.appendChild(dateLblEl);
+
+  const dateValEl = document.createElement('p');
+  dateValEl.classList.add('card__value', 'card__value--bold');
+  dateValEl.innerText = new Date(show.date).toDateString();
+  cardEl.appendChild(dateValEl);
+
+  const venueLblEl = document.createElement('p');
+  venueLblEl.classList.add('card__label', 'labels');
+  venueLblEl.innerText = 'VENUE';
+  cardEl.appendChild(venueLblEl);
+
+  const venueValEl = document.createElement('p');
+  venueValEl.classList.add('card__value');
+  venueValEl.innerText = show.place;
+  cardEl.appendChild(venueValEl);
+
+  const locationLblEl = document.createElement('p');
+  locationLblEl.classList.add('card__label', 'labels');
+  locationLblEl.innerText = 'LOCATION';
+  cardEl.appendChild(locationLblEl);
+
+  const locationValEl = document.createElement('p');
+  locationValEl.classList.add('card__value');
+  locationValEl.innerText = show.location;
+  cardEl.appendChild(locationValEl);
+
+  const buttonEl = document.createElement('a');
+  buttonEl.classList.add('card__button', 'button');
+  buttonEl.setAttribute('href', '#');
+  buttonEl.innerText = 'BUY TICKETS';
+  cardEl.appendChild(buttonEl);
+
+  return cardEl;
+}
 
 //EVENTS
-
-
-
-
-
-
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,9 +3,10 @@ const SHOWS_ENDPOINT = "https://project-1-api.herokuapp.com/comments?api_key=";
 const API_KEY = "e0eea5f0-0f8c-4b54-9fc4-ff50843766d4";
 
 const formEl = document.querySelector("form");
-const cardList = document.querySelector(".comment-cards");
-const commentInput = document.querySelector(".comment-section__comment-input");
-const nameInput = document.querySelector(".comment-section__name-input");
+const cardList = document.querySelector(".comment__cards");
+const commentInput = document.querySelector(".comment__comment-input");
+const nameInput = document.querySelector(".comment__name-input");
+
 //FUNCTIONS
 function loadComments() {
 
@@ -20,7 +21,7 @@ axios
     failMsg = document.createElement("p");
     failMsg.innerText =
       "We're sorry, but there was an issue loading the data for this section.Please refresh the page to try again.";
-    failMsg.classList.add("comment-cards__error-msg");
+    failMsg.classList.add("comment__error-msg");
     cardList.appendChild(failMsg); // not showing
   });
 
@@ -37,38 +38,32 @@ function displayComment(comments) {
 
 //take a comment object, create a card element, append child elements and return card element
 function createComment(comment) {
-  //<article class="comment-card">
+ 
   const cardEl = document.createElement("article");
   cardEl.classList.add("comment-card");
 
-  //<img class="comment-card__avatar">
   const avatarEl = document.createElement("div");
   avatarEl.classList.add("comment-card__avatar");
   cardEl.appendChild(avatarEl);
 
-  //<div class="comment-card__content">
   const contentEl = document.createElement("div");
   contentEl.classList.add("comment-card__content");
   cardEl.appendChild(contentEl);
 
-  //<div class="comment-card__name-box">
   const nameBoxEl = document.createElement("div");
   nameBoxEl.classList.add("comment-card__name-box");
   contentEl.appendChild(nameBoxEl);
 
-  //<h3 class="comment-card__name">Connor Walton</h3>
   const nameEl = document.createElement("h3");
   nameEl.classList.add("comment-card__name");
   nameEl.innerText = comment.name;
   nameBoxEl.appendChild(nameEl);
 
-  //<p class="comment-card__date">02/17/2021</p>
   const dateEl = document.createElement("p");
   dateEl.classList.add("comment-card__date");
   dateEl.innerText = new Date(comment.timestamp).toLocaleDateString("en-US");
   nameBoxEl.appendChild(dateEl);
 
-  //<p class="comment-card__text">
   const textEl = document.createElement("p");
   textEl.classList.add("comment-card__text");
   textEl.innerText = comment.comment;
@@ -83,18 +78,14 @@ function validateEntries(name, comment) {
 
   if (!name.trim()) {
     isValid = false;
-    nameInput.classList.add("comment-section__input--invalid");
+    nameInput.classList.add("comment__input--invalid");
   }
 
   if (!comment.trim()) {
     isValid = false;
-    commentInput.classList.add("comment-section__input--invalid");
+    commentInput.classList.add("comment__input--invalid");
   }
-
-  if (isValid === false) {
-    return false;
-  }
-  return true;
+  return isValid;
 }
 
 //EVENTS
@@ -102,14 +93,14 @@ function validateEntries(name, comment) {
 //Delete invalid class
 
 nameInput.addEventListener("focus", (event) => {
-  if (nameInput.classList.contains("comment-section__input--invalid")) {
-    nameInput.classList.remove("comment-section__input--invalid");
+  if (nameInput.classList.contains("comment__input--invalid")) {
+    nameInput.classList.remove("comment__input--invalid");
   }
 });
 
 commentInput.addEventListener("focus", (event) => {
-  if (commentInput.classList.contains("comment-section__input--invalid")) {
-    commentInput.classList.remove("comment-section__input--invalid");
+  if (commentInput.classList.contains("comment__input--invalid")) {
+    commentInput.classList.remove("comment__input--invalid");
   }
 });
 
@@ -122,10 +113,11 @@ formEl.addEventListener("submit", (e) => {
   const comment = e.target.comment.value;
 
   isValid = validateEntries(name, comment);
-
-  if (isValid === false) {
+  
+  if (!isValid) {
     return false;
   }
+
   //Clear the input fields after submitting a new comment
   e.target.name.value = "";
   e.target.comment.value = "";
@@ -142,15 +134,17 @@ formEl.addEventListener("submit", (e) => {
       loadComments();
     })
 
-    .catch((error) => {
-      console.log(error);
-      // We encountered a problem while submitting your comment. Please refresh the page and try again.
+    .catch(() => {
+      failMsg = document.createElement('p');
+      failMsg.innerText = "We encountered a problem while submitting your comment. Please refresh the page and try again.";
+      failMsg.classList.add('comment__error-msg');
+      formEl.appendChild(failMsg);
+  
     });
 });
 
 // //Invoked funcs when loading
 loadComments();
 
-// displayComment();
 
-// // test
+
